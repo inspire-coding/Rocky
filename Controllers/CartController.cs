6012c8a16b9f40243574caf9ba9a329eed7f8b20
inspire-coding <+PaZ+PeTi+88113+>
2021-04-06 16:39:12 +0200

    07. Shopping Cart and Inquiry Email

---
+++ Controllers/CartController.cs
@@ -0,0 +1,143 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Rocky.Data;
+using Rocky.Models;
+using Rocky.Models.ViewModels;
+using Rocky.Utility;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+
+namespace Rocky.Controllers
+{
+    [Authorize]
+    public class CartController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        private readonly IWebHostEnvironment _webHostEnvironment;
+
+        [BindProperty]
+        private ProductUserViewModel ProductUserViewModel { get; set; }
+
+        public CartController(ApplicationDbContext db, IWebHostEnvironment webHostEnvironment)
+        {
+            _db = db;
+            _webHostEnvironment = webHostEnvironment;
+        }
+
+        public IActionResult Index()
+        {
+            List<ShoppingCart> shoppingCarts = new List<ShoppingCart>();
+
+            if (HttpContext.Session.Get<List<ShoppingCart>>(WC.SessionCart) != null
+                && HttpContext.Session.Get<List<ShoppingCart>>(WC.SessionCart).Count() > 0)
+            {
+                // Session exists
+                shoppingCarts = HttpContext.Session.Get<List<ShoppingCart>>(WC.SessionCart);
+            }
+
+            List<int> prodInCart = shoppingCarts.Select(i => i.ProductId).ToList();
+            IEnumerable<Product> prodList = _db.Products.Where(u => prodInCart.Contains(u.Id));
+
+            return View(prodList);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [ActionName("Index")]
+        public IActionResult IndexPost()
+        {
+
+
+            return RedirectToAction(nameof(Summary));
+        }
+
+        public IActionResult Summary()
+        {
+            var claimsIdentity = (ClaimsIdentity)User.Identity;
+            var claim = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier);
+
+
+            List<ShoppingCart> shoppingCarts = new List<ShoppingCart>();
+
+            if (HttpContext.Session.Get<List<ShoppingCart>>(WC.SessionCart) != null
+                && HttpContext.Session.Get<List<ShoppingCart>>(WC.SessionCart).Count() > 0)
+            {
+                // Session exists
+                shoppingCarts = HttpContext.Session.Get<List<ShoppingCart>>(WC.SessionCart);
+            }
+
+            List<int> prodInCart = shoppingCarts.Select(i => i.ProductId).ToList();
+            IEnumerable<Product> prodList = _db.Products.Where(u => prodInCart.Contains(u.Id));
+
+            ProductUserViewModel = new ProductUserViewModel()
+            {
+                ApplicationUser = _db.ApplicationUsers.FirstOrDefault(u => u.Id == claim.Value),
+                Products = prodList.ToList()
+            };
+
+            return View(ProductUserViewModel);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [ActionName("Summary")]
+        public IActionResult SummaryPost(ProductUserViewModel productUserViewModel)
+        {
+            var PathToTemplate = _webHostEnvironment.WebRootPath + Path.DirectorySeparatorChar.ToString()
+                 + "templates" + Path.DirectorySeparatorChar.ToString()
+                 + "Inquiry.html";
+
+            var subject = "New Inquiry";
+            string HtmlBody = "";
+
+            using (StreamReader sr = System.IO.File.OpenText(PathToTemplate))
+            {
+                HtmlBody = sr.ReadToEnd();
+            }
+
+            StringBuilder productListSB = new StringBuilder();
+            foreach (var prod in ProductUserViewModel.Products)
+            {
+                productListSB.Append($" - Name: { prod.Name } <span style='font-size:14px;'> (ID: { prod.Id })</span><br />");
+            }
+
+            string messageBody = string.Format(HtmlBody,
+                productUserViewModel.ApplicationUser.FullName,
+                productUserViewModel.ApplicationUser.Email,
+                productUserViewModel.ApplicationUser.PhoneNumber,
+                productListSB.ToString());
+            
+            return RedirectToAction(nameof(InquiryConfirmation));
+        }
+
+        public IActionResult InquiryConfirmation()
+        {
+            HttpContext.Session.Clear();
+
+            return View();
+        }
+
+        public IActionResult Remove(int id)
+        {
+            List<ShoppingCart> shoppingCarts = new List<ShoppingCart>();
+
+            if (HttpContext.Session.Get<List<ShoppingCart>>(WC.SessionCart) != null
+                && HttpContext.Session.Get<List<ShoppingCart>>(WC.SessionCart).Count() > 0)
+            {
+                // Session exists
+                shoppingCarts = HttpContext.Session.Get<List<ShoppingCart>>(WC.SessionCart);
+            }
+
+            shoppingCarts.Remove(shoppingCarts.FirstOrDefault(u => u.ProductId == id));
+
+            HttpContext.Session.Set(WC.SessionCart, shoppingCarts);
+
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
---
+++ Models/ViewModels/ProductUserViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace Rocky.Models.ViewModels
+{
+    public class ProductUserViewModel
+    {
+        public ProductUserViewModel()
+        {
+            Products = new List<Product>();
+        }
+
+        public ApplicationUser ApplicationUser { get; set; }
+        public IList<Product> Products { get; set; }
+    }
+}
